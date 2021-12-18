3e81af1196236da3b2519b4afcbce8ad05ad4f3d
Shabab Khan <shababkhanSWE@gmail.com>
2021-12-18 15:08:32 -0500

    added homepage link to each page

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -1,8 +1,10 @@
-import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 
 const useStyles = makeStyles( () => ({
@@ -15,10 +17,13 @@ const useStyles = makeStyles( () => ({
   title: {
     flexGrow: 1,
     textAlign: 'left',
-    textDecoration: 'none'
-  }, 
-  customizeAppBar:{
-    backgroundColor: '#11153e',
+    fontType: 'bold',
+    fontFamily: 'Helvetica, sans-serif', 
+    fontSize: '35px', 
+    color: '#FFFFFF'
+  },
+  appBar:{
+    backgroundColor: '#eab676',
     shadows: ['none'],
   },
   formTitle:{
@@ -36,7 +41,28 @@ const NewCampusView = (props) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} style={{textDecoration: 'none'}}>
+        <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          <Link className={classes.title} to={'/'}>
+            <Typography variant="h6" className={classes.title} color="inherit" >
+                CampusHub
+            </Typography>
+          </Link>
+          
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              All Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <div className={classes.formContainer}>
         <div className={classes.formTitle}>
           <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
