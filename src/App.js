709037ba8618e7c7cb9e2d757e5d7243a25b8a44
Adam <73608454+adamjoshuamohammad@users.noreply.github.com>
2021-12-19 17:25:18 -0500

    added the import for EditCampusContainer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import {
   AllStudentsContainer,
   NewCampusContainer,
   NewStudentContainer,
-  EditStudentContainer
+  EditStudentContainer,
+  EditCampusContainer
 } from './components/containers';
 
 // if you create separate components for adding/editing 
