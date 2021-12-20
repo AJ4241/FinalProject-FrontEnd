45c49c74d0b22218b29c6456b1cfe6556f48ef55
xlin6798 <xlin6798@gmail.com>
2021-12-20 16:48:53 -0500

    Fix edit student

--- src/components/containers/EditStudentContainer.js
+++ src/components/containers/EditStudentContainer.js
@@ -1,86 +1,87 @@
-import { Component } from "react";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-import EditStudentView from "../views/EditStudentView";
-import { editStudentThunk, fetchStudentThunk } from "../../store/thunks";
+import EditStudentView from '../views/EditStudentView';
+import { editStudentThunk, fetchStudentThunk } from '../../store/thunks';
+
 
 class EditStudentContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstname: "",
-      lastname: "",
-      email: "",
-      gpa: 0,
-      campusId: null,
-      redirect: false,
-      redirectId: null,
-    };
-  }
-
-  componentDidMount() {
-    this.props.fetchStudent(window.location.pathname.slice(-1));
-
-    this.setState({
-      firstname: this.props.student.firstname,
-      lastname: this.props.student.lastname,
-      email: this.props.student.email,
-      gpa: this.props.student.gpa,
-      campusId: this.props.student.campusId,
-      redirect: false,
-      redirectId: null,
-      id: this.props.student.id,
-    });
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-
-    let editStudent = {
-      firstname: this.state.firstname,
-      lastname: this.state.lastname,
-      email: this.state.email,
-      gpa: this.state.gpa,
-      campusId: this.state.campusId,
-      id: window.location.pathname.slice(-1),
-    };
-
-    await this.props.editStudent(editStudent);
-
-    this.setState({
-      firstname: "",
-      lastname: "",
-      email: "",
-      gpa: 0,
-      campusId: null,
-      redirect: true,
-      redirectId: window.location.pathname.slice(-1),
-    });
-  };
-
-  componentWillUnmount() {
-    this.setState({ redirect: false, redirectId: null });
-  }
-
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to={`/student/${this.state.redirectId}`} />;
+    constructor(props){
+        super(props);
+        this.state = {
+          firstname: "", 
+          lastname: "",
+          email: "",
+          gpa: 0, 
+          campusId: null, 
+          redirect: false, 
+          redirectId: null,
+          id: null,
+        };
     }
-    return (
-      <EditStudentView
-        handleChange={this.handleChange}
-        handleSubmit={this.handleSubmit}
-        student={this.props.student}
-      />
-    );
-  }
+
+    componentDidMount() {
+      this.props.fetchStudent(window.location.pathname.slice(-1));
+
+      this.setState({
+        firstname: this.props.student.firstname,
+        lastname: this.props.student.lastname,
+        email: this.props.student.email,
+        gpa: this.props.student.gpa,
+        campusId: this.props.student.campusId,
+        redirect: false,
+        redirectId: null,
+      })
+    }
+
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit = async (event, sid) => {
+        event.preventDefault();
+
+        let editStudent = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            email: this.state.email,
+            gpa: this.state.gpa,
+            campusId: this.state.campusId,
+            id: sid,
+        };
+        
+        await this.props.editStudent(editStudent);
+
+        this.setState({
+          firstname: "", 
+          lastname: "", 
+          email: "",
+          gpa: 0,
+          campusId: null, 
+          redirect: true, 
+          redirectId: sid,
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({redirect: false, redirectId: null});
+    }
+
+    render() {
+        if(this.state.redirect) {
+          return (<Redirect to={`/student/${this.state.redirectId}`}/>)
+        }
+        return (
+          <EditStudentView 
+            handleChange = {this.handleChange} 
+            handleSubmit = {this.handleSubmit}
+            student = {this.props.student}    
+          />
+        );
+    }
 }
 
 const mapState = (state) => {
@@ -90,10 +91,10 @@ const mapState = (state) => {
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    editStudent: (student) => dispatch(editStudentThunk(student)),
-    fetchStudent: (student) => dispatch(fetchStudentThunk(student)),
-  };
-};
+    return({
+        editStudent: (student) => dispatch(editStudentThunk(student)),
+        fetchStudent: (student) => dispatch(fetchStudentThunk(student)),
+    })
+}
 
 export default connect(mapState, mapDispatch)(EditStudentContainer);
